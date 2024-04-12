610dc4df94e9a266498221e942587c71e608daa3
Lorenzo <vore@zhaw.ch>
2024-04-12 10:32:46 +0200

    Added  basic functionality to app

--- final_project/paekli-cli/src/main.rs
+++ final_project/paekli-cli/src/main.rs
@@ -1,10 +1,34 @@
-use clap::Parser;
+use clap::{Parser, Subcommand};
+
 /// send and receive joy with ✨ paekli-cli ✨
-#[derive(Parser)]
 #[clap(version)]
-struct Cli;
+
+#[derive(Subcommand)]
+enum Command {
+    Send,
+    Receive,
+}
+
+#[derive(Parser)]
+struct Cli{
+    #[command(subcommand)]
+    command: Command,
+}
+
+const SEND_MESSAGE: &str = "\
+Thank you for trusting Paekli LLC!
+We will deliver your paekli in mint condition.
+* throws your paekli directly in the trash *";
+
+const RECEIVE_MESSAGE: &str = "\
+There aren't any paekli for you at the moment.
+* tries to hide paekli in the trash can *";
 
 fn main() {
-    let _args = Cli::parse();
-    println!("Paekli LLC is currentli closed 😢");
+    let args = Cli::parse();
+    
+    match args.command {
+        Command::Send => println!("{SEND_MESSAGE}"),
+        Command::Receive => println!("{RECEIVE_MESSAGE}")
+    }
 }
