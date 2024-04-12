38a2bd263171391752f74b04303e454c0ddad4c4
Lorenzo <vore@zhaw.ch>
2024-04-12 11:11:32 +0200

    Added real sending and receiving functions including storage solution

--- final_project/paekli-cli/src/main.rs
+++ final_project/paekli-cli/src/main.rs
@@ -1,34 +1,49 @@
+use anyhow::Context;
 use clap::{Parser, Subcommand};
-
+use std::fs::{self, read_to_string};
+use std::{fs::read, path::Path};
 /// send and receive joy with ✨ paekli-cli ✨
-#[clap(version)]
 
 #[derive(Subcommand)]
 enum Command {
-    Send,
+    Send { content: String },
     Receive,
 }
 
 #[derive(Parser)]
-struct Cli{
+#[clap(version)]
+struct Cli {
     #[command(subcommand)]
     command: Command,
 }
-
-const SEND_MESSAGE: &str = "\
-Thank you for trusting Paekli LLC!
-We will deliver your paekli in mint condition.
-* throws your paekli directly in the trash *";
-
-const RECEIVE_MESSAGE: &str = "\
-There aren't any paekli for you at the moment.
-* tries to hide paekli in the trash can *";
-
-fn main() {
+fn main() -> anyhow::Result<()> {
     let args = Cli::parse();
-    
+    let project_dir = directories::ProjectDirs::from("dev", "buenzli", "paekli")
+        .context("the user's home directory seems to be corrupt")?;
+    let storage_dir = project_dir.data_dir();
+    std::fs::create_dir_all(storage_dir).expect("faield to create storage directory");
+
     match args.command {
-        Command::Send => println!("{SEND_MESSAGE}"),
-        Command::Receive => println!("{RECEIVE_MESSAGE}")
+        Command::Send { content } => {
+            if !Path::exists(&storage_dir.join("content")) {
+                std::fs::write(storage_dir.join("content"), content)
+                    .context("failed to store paekli")?;
+            } else {
+                return Err(anyhow::anyhow!("Paekli Storage is full"));
+            }
+        }
+        Command::Receive => {
+            if Path::exists(&storage_dir.join("content")) {
+                match read_to_string(storage_dir.join("content")) {
+                    Ok(contents) => println!("Paekli content: \n{}", contents),
+                    Err(e) => println!("Error reading Paekli Storage: {}", e),
+                }
+                std::fs::remove_file(storage_dir.join("content"))
+                    .context("failed to remove storage")?;
+            } else {
+                return Err(anyhow::anyhow!("Paekli Storage is empty :("));
+            }
+        }
     }
+    Ok(())
 }
