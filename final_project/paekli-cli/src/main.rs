a2a4eb9510e0725d926f28ba73cce685984fd681
Lorenzo <vore@zhaw.ch>
2024-04-12 10:25:05 +0200

    added documentation and version information to app

--- final_project/paekli-cli/src/main.rs
+++ final_project/paekli-cli/src/main.rs
@@ -1,3 +1,10 @@
+use clap::Parser;
+/// send and receive joy with ✨ paekli-cli ✨
+#[derive(Parser)]
+#[clap(version)]
+struct Cli;
+
 fn main() {
+    let _args = Cli::parse();
     println!("Paekli LLC is currentli closed 😢");
 }
